e34128be3a606a8b5a0a94cfffadf852eb518d26
Billal Fauzan <57545923+billal1412@users.noreply.github.com>
2019-12-09 12:55:34 +0700

    Add files via upload

--- md5.py
+++ md5.py
@@ -12,18 +12,19 @@ NOTE:
 	Jadi
 	Jangan Di Recode Bangsat!!
 """
-__banner__ = """
+__banner__ = """\033[31;1m
     __  __           __    ____
    / / / /___ ______/ /_  / __ \___  _____
-  / /_/ / __ `/ ___/ __ \/ / / / _ \/ ___/
+  / /_/ / __ `/ ___/ __ \/ / / / _ \/ ___/\033[37;1m
  / __  / /_/ (__  ) / / / /_/ /  __/ /__
-/_/ /_/\__,_/____/_/ /_/_____/\___/\___/
+/_/ /_/\__,_/____/_/ /_/_____/\___/\___/\033[33;1m
      copyright 2019 Billal Fauzan
-
-INFO:
-    Author : Billal Fauzan
-    Version: 1.0
-    Name   : Hash Dencrypter Offline 50%
+\033[32;1m
+INFO:\033[34;1m
+    Author \033[31;1m:\033[32;1m Billal Fauzan\033[34;1m
+    Version\033[31;1m:\033[32;1m 1.0\033[34;1m
+    Name  \033[31;1m :\033[32;1m Hash Dencrypter Offline 50%\033[34;1m
+    Link To Download \033[31;1m:\033[32;1m bit.ly/passwordhashdec\033[0m
 
 """
 try:
@@ -33,17 +34,23 @@ except Exceptions as E:
 	sys.exit()
 
 def dencrypt(hash,password):
-	sys.stdout.write("\r\n[!] Trying: "+str(password))
-	sys.stdout.flush()
-	time.sleep(0.1)
-	md5 = hashlib.md5()
-	md5.update(str(password))
-	md = md5.hexdigest()
-	if hash == md:
-		print ("\n[+] Found: "+str(password))
+	try:
+		sys.stdout.write("\n\033[34;1m[\033[33;1m!\033[34;1m] \033[33;1mTrying: \033[31;1m"+str(password))
+		sys.stdout.flush()
+#	time.sleep(0.1)
+		md5 = hashlib.md5()
+		md5.update(str(password))
+		md = md5.hexdigest()
+		if hash == md:
+			print ("\033[34;1m\n[\033[32;1m+\033[34;1m] \033[37;1mFound: \033[32;1m"+str(password))
+			print ("\033[0m")
+			sys.exit()
+	except:
+		print ("\033[31;1m[-] unknown error\033[0m")
 		sys.exit()
 
 def main():
+	os.system("clear")
 	print (__banner__)
 	hash = raw_input("[?] Hash: ")
 	wordlist =""
@@ -62,4 +69,18 @@ def main():
 	except IOError:
 		print ("[Err]: File %s not found"%(wordlist))
 		sys.exit()
-main()
+
+def login():
+	os.system("clear")
+	print (__banner__)
+	pwd = "6d845a610cf2ff6872a04239349251e4"
+	print ("[*] Please contact author to find out the password")
+	pw = raw_input("[?] Password to unlock: ")
+	m = hashlib.md5()
+	m.update(pw)
+	md = m.hexdigest()
+	if pwd == md:
+		main()
+	else:
+		print ("[*] Please contact author to find out the password")
+login()
